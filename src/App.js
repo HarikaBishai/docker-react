8f497acf04e7c87527d3f8973630e0703b314042
Harika Bishai <harikabishai@Harikas-MacBook-Air.local>
2025-03-24 19:49:59 -0400

    text changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello there! I am a App.
+          learn react
         </a>
       </header>
     </div>
